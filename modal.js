d28c34221282c1477c62c8478a6a9a5f272fca82
Bayan14 <123265085+Bayan14@users.noreply.github.com>
2023-01-22 00:23:53 +0600

    Project bn

---
+++ modal.js
@@ -0,0 +1,7 @@
+let modal = document.querySelector('#modal-window');
+function openModal(){
+    modal.style.display = 'flex'
+}
+function closeModal(){
+    modal.style.display = 'none'
+}
---
+++ slider.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+
+// $('.slider').slick();
